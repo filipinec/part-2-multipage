60985ac4089c25bea127cc8072114e44006c1aac
Filip Tasevski <135996963+filipinec@users.noreply.github.com>
2024-03-21 21:47:46 +0100

    Create Router

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero neque odio modi facilis quibusdam! Placeat laudantium sed officiis aspernatur ullam qui similique. Praesentium ullam rerum molestias iusto commodi, voluptate minima.</p>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero neque odio modi facilis quibusdam! Placeat laudantium sed officiis aspernatur ullam qui similique. Praesentium ullam rerum molestias iusto commodi, voluptate minima.</p>
+    </div>
+  )
+}
+
+export default Home
